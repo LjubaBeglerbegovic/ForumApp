badb44ba94a451184cc8d43bc3f40f555729e444
Ljuba Beglerbegovic <ljuba93b@gmail.com>
2019-04-09 11:30:15 -0700

    Add Post class

--- src/main/java/com/example/forum/model/Post.java
+++ src/main/java/com/example/forum/model/Post.java
@@ -1,5 +1,37 @@
 package com.example.forum.model;
 
-public class Post {
+import javax.persistence.*;
 
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+
+@Data
+@NoArgsConstructor
+@Entity
+public class Post {
+	
+	public Post(String subject, String message) {
+		if(message == null) {
+			throw new NullPointerException("Message must not be null!");
+		}
+		
+		this.subject = subject;
+		this.message = message;
+	}
+	
+	@GeneratedValue
+	@Id
+	private Long id;
+	@NonNull
+	private String subject;
+	private String message;
+	
+	public String getId() {
+		if(id==null) {
+			return "";
+		}
+		return id.toString();
+	}
+	
 }
